f453aab1242232c0aa7e89c970398307cd5af879
alexanderUmnikov <strife88@mail.ru>
2018-08-29 18:05:28 +1000

    WiP

--- newScript.js
+++ newScript.js
@@ -1,32 +1,39 @@
 
 
-var burst = {
- "FireRounds" : 3,
- "ApCost" : {
- "precise" : 3,
- "normal" : 2,
- "fast" : 1,
+var pistol = {
+ BurstRounds : 3,
+ ApCost : {
+ precise : 3,
+ normal : 2,
+ fast : 1,
 },
- "hitChance" : {
-  "precise" : 0.3,
-  "normal" : 0.2,
-  "fast" : 0.1,
- }
+ hitChance : {
+ precise : 30,
+ normal : 20,
+ fast : 10,
+},
+ PreciseFire : fire(this.hitChance.precise),
+ normalFire : fire(this.hitChance.normal),
+ fastFire : fire(this.hitChance.fast)
+ PreciseBurst : fire(this.hitChance.precise),
+ normalBurst : fire(this.hitChance.normal),
+ fastBurst : fire(this.hitChance.fast)
+  }
 }
 
 var single = {
- "FireRounds" : 1,
- "ApCost" : {
- "precise" : 3,
- "normal" : 2,
- "fast" : 1,
+ FireRounds : 1,
+ ApCost : {
+ precise : 3,
+ normal : 2,
+ fast : 1,
 },
- "hitChance" : {
-  "precise" : 0.7,
-  "normal" : 0.5,
-  "fast" : 0.3,
+ hitChance : {
+ precise : 0.7,
+ normal : 0.5,
+ fast : 0.3,
 },
-  
+
 }
 
 
